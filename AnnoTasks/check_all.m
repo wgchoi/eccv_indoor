643e6285175544962d1d4ece4c73ea018e9d634c
Wongun Choi <wgchoi@gmail.com>
2012-02-06 22:39:58 -0500

    set7 livingroom added

--- AnnoTasks/check_all.m
+++ AnnoTasks/check_all.m
@@ -2,10 +2,10 @@ clear
 
 corrector = 'wongun';
 
-imbase = 'livingroom/set6/';
+imbase = 'livingroom/set7/';
 
-annobase = 'annotation/temp/set6/';
-verifiedbase = 'annotation/livingroom/set6/';
+annobase = 'annotation/temp/set7/';
+verifiedbase = 'annotation/livingroom/set7/';
 
 imfiles = dir(fullfile(imbase, '*.JPEG'));
 
@@ -19,7 +19,7 @@ for i = 1:length(imfiles)
     
     annofile = [imfiles(i).name(1:idx-1) '_labels.mat'];
     
-    orgfile = fullfile(annobase, annofile);
+    orgfile = fullfile(annobase, [imfiles(i).name(1:end-4) '_labels.mat']);
     destfile = fullfile(verifiedbase, annofile);
     if exist(destfile, 'file')
         orgfile = destfile;
