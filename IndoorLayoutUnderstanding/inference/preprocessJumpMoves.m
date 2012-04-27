3166940d13370f88948166ca60a76960d0879ed1
Wongun Choi <wgchoi@gmail.com>
2012-04-27 16:20:54 -0400

    sample data added

--- IndoorLayoutUnderstanding/inference/preprocessJumpMoves.m
+++ IndoorLayoutUnderstanding/inference/preprocessJumpMoves.m
@@ -1,2 +1,15 @@
-function preprocessJumpMoves()
+function moves = preprocessJumpMoves(x, iclusters)
+
+% moves : for each jump type : 
+%       add, delete,            % no cahce required.
+%       switch, combine, break
+%       1. src cluster ID
+%       2. dest cluster ID
+%       3. required cluster IDs
+%       4. cache for liklihood computation?
+
+moves = cell(5, 1);
+for i = 1:length(iclusters)
+end
+
 end
