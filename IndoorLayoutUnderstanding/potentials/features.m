21d3595fe26a4a3d49ce56031bef77588fe942f0
Wongun Choi <wgchoi@napoli6.(none)>
2012-05-28 17:18:24 -0400

    UIUC_CVPR data conversion

--- IndoorLayoutUnderstanding/potentials/features.m
+++ IndoorLayoutUnderstanding/potentials/features.m
@@ -11,6 +11,8 @@ if( isfield(model, 'commonground') && model.commonground )
         phi = features6(pg, x, iclusters, model);
     elseif(strcmp(model.feattype, 'itm_v0'))
         phi = features_itm0(pg, x, iclusters, model);
+    elseif(strcmp(model.feattype, 'itm_v1'))
+        phi = features_itm1(pg, x, iclusters, model);
     end
     return;
 end
@@ -20,6 +22,126 @@ if(~isfield(model, 'feattype') || strcmp(model.feattype, 'type1'))
 elseif(strcmp(model.feattype, 'type2'))
     phi = features2(pg, x, iclusters, model);
 end
+
+end
+
+function phi = features_itm1(pg, x, iclusters, model)
+
+featlen =   1 + ... % scene classification 
+            1 + ... % layout confidence : no bias required, selection problem    
+            2 * model.nobjs + ... % object confidence : (weight + bias) per type
+            model.nobjs * ( length(model.ow_edge) - 1 ) + ... % object-wall inclusion 
+            ( model.nobjs * model.nscene ) + ... % semantic constext
+            sum(model.itmfeatlen) + ... % intearction templates!
+            2 + ... % object-object interaction : 2D bboverlap, 2D polyoverlap
+            model.nobjs + ...         % projection-deformation cost
+            1;              % floor distance
+
+phi = zeros(featlen, 1);
+ibase = 1;
+
+objidx = getObjIndices(pg, iclusters);
+
+assert(isfield(pg, 'objscale'));
+cubes = cell(1, length(objidx));
+for i = 1:length(objidx)
+    idx = objidx(i);
+    cubes{i} = x.cubes{idx} .* pg.objscale(i);
+end
+
+%% scene classification
+phi(ibase) = x.sconf(pg.scenetype);
+ibase = ibase + 1;
+%% scene layout confidence
+phi(ibase) = x.lconf(pg.layoutidx);
+ibase = ibase + 1;
+%% object observation confidence + bias
+for i = 1:length(objidx)
+    i1 = objidx(i);
+    if(iclusters(i1).isterminal)
+        obase = (iclusters(i1).ittype - 1) * 2;
+        
+        phi(ibase + obase) = phi(ibase + obase) + x.dets(i1, 8); % detection confidence
+        phi(ibase + obase + 1) = phi(ibase + obase + 1) + 1;
+    else
+        assert(false, 'not ready');
+    end
+end
+ibase = ibase + 2 * model.nobjs;
+%% object-wall interaction - no inclusion
+nbin = (length(model.ow_edge) - 1);
+for i = 1:length(cubes)
+    %%%%%% need to make it robust!!!
+    volume = cuboidRoomIntersection(x.faces{pg.layoutidx}, pg.camheight, cubes{i});
+    temp = histc(volume(2:4), model.ow_edge);
+    i1 = objidx(i);
+    if(iclusters(i1).isterminal)
+        idx = ibase + nbin * (iclusters(i1).ittype - 1);
+        phi(idx:idx+nbin-1) = phi(idx:idx+nbin-1) + temp(1:end-1);
+    else
+        assert(false, 'not right');
+    end
+end
+ibase = ibase + model.nobjs * nbin;
+%% object scene context
+sidx = (pg.scenetype - 1) * model.nobjs;
+for i = 1:length(objidx)
+    i1 = objidx(i);
+    if(iclusters(i1).isterminal)
+        idx = ibase + sidx + iclusters(i1).ittype - 1;
+        phi(idx) = phi(idx) + 1;
+    else
+        assert(false, 'not ready');
+    end
+end
+ibase = ibase + model.nobjs * model.nscene;
+%% interaction templates!
+for i = 1:length(pg.childs)
+    i1 = pg.childs(i);
+    if(~iclusters(i1).isterminal)
+        itmid = iclusters(i1).ittype - model.nobjs;
+        % compute itm features
+        temp = ibase + model.itmbase(itmid);
+        locs = zeros(length(iclusters(i1).chindices), 4);
+        for j = 1:length(iclusters(i1).chindices)
+            idx = find(objidx == iclusters(i1).chindices(j), 1);
+            
+            locs(j, 1:3) = x.locs(iclusters(i1).chindices(j), 1:3) * pg.objscale(idx);
+            locs(j, 4) = x.locs(iclusters(i1).chindices(j), 4);
+        end
+        % 
+        itmfeat = getITMfeat(model.itmptns(itmid), locs, model);
+        phi(temp:temp+model.itmfeatlen(itmid)-1) = itmfeat;
+    end
+end
+ibase = ibase + sum(model.itmfeatlen);
+%% overlap between a pair of objects
+phi(ibase) = sum(sum(x.orarea(objidx, objidx)));
+phi(ibase + 1) = sum(sum(x.orpolys(objidx, objidx)));
+ibase = ibase + 2;
+
+%% object scale deformation
+objscale = pg.objscale;
+objscale(objscale < 0) = 1e-2; % safe guard to avoid error
+for i = 1:length(objidx)
+    i1 = objidx(i);
+    if(iclusters(i1).isterminal)
+        idx = ibase + iclusters(i1).ittype - 1;
+        phi(idx) = phi(idx) + ( log(objscale(i)) ) .^ 2;
+    else
+        assert(false, 'not right');
+    end
+end
+ibase = ibase + model.nobjs;
+%% object-floor interaction 
+for i = 1:length(objidx)
+    bottom = min(cubes{i}(2, :)); % bottom y position.
+    phi(ibase) = phi(ibase) + (pg.camheight + bottom) .^ 2; %
+end
+ibase = ibase + 1;
+
+assert(featlen == ibase - 1);
+assert(~(any(isnan(phi)) || any(isinf(phi))));
 
 end
 
---
+++ UIUC_CVPR_dataset/generateAnnotations.m
@@ -0,0 +1,84 @@
+addpath ../IndoorLayoutUnderstanding/objmodel/
+addpath ../IndoorLayoutUnderstanding/common/
+
+imdir = 'Images';
+imfiles = dir(fullfile(imdir, '*.jpg'));
+
+layoutdir = 'GTSpatiallayout/';
+objdir = 'GT3dcuboids';
+annodir = 'Annotations';
+
+annos= struct('objtypes', [], 'gtPolyg', [], ...
+                'objmodel', [], 'obj_annos', struct('im', [], 'objtype', 0, 'subid', 0, 'x1', 0, 'x2', 0, 'y1', 0, 'y2', 0, 'azimuth', 0, 'elevation', 0));
+
+objtypes =  {'Sofa'  'Table'  'Chair'  'Bed' 'Dining Table' 'Side Table'};
+
+temp = {};
+for i = 1:length(imfiles)
+    layoutfile = [imfiles(i).name(1:find(imfiles(i).name =='.', 1, 'last')-1) '_labels'];
+    objfile = [imfiles(i).name(1:find(imfiles(i).name =='.', 1, 'last')-1) '_box'];
+    annofile = layoutfile;
+    
+    annos.objtypes = objtypes;
+    annos.gtPolyg = cell(1, 5);
+    annos.objmodel = objmodels();
+    annos.obj_annos(:) = [];
+
+    try
+        layout = load(fullfile(layoutdir, layoutfile));
+    catch
+        continue;
+    end
+    
+    for j = 1:length(layout.gtPolyg)
+        annos.gtPolyg{j} = layout.gtPolyg{j};
+    end
+    
+    try
+        objs = load(fullfile(objdir, objfile));
+        for j = 1:length(objs.annotation)
+            if(strcmp(objs.annotation(j).label, 'sofa'))
+                type = 1;
+                subtype = 1;
+            elseif(strcmp(objs.annotation(j).label, 'table'))
+                type = 2;
+                subtype = 1;
+            elseif(strcmp(objs.annotation(j).label, 'bed'))
+                type = 4;
+                subtype = 1;
+            elseif(strcmp(objs.annotation(j).label, 'drawer'))
+                type = 6;
+                subtype = 1;
+            elseif(strcmp(objs.annotation(j).label, 'chair'))
+                type = 3;
+                subtype = 1;
+            elseif(strcmp(objs.annotation(j).label, 'sofa chair'))
+                type = 1;
+                subtype = 2;
+            end
+            
+            bbox = [min(objs.annotation(j).fullbox(:, 1)), ...
+                    min(objs.annotation(j).fullbox(:, 2)), ...
+                    max(objs.annotation(j).fullbox(:, 1)), ...
+                    max(objs.annotation(j).fullbox(:, 2))];
+            
+            obj = struct('im', fullfile(imdir, imfiles(i).name), ...
+                        'objtype', type, ...
+                        'subid', subtype, ...
+                        'x1', min(objs.annotation(j).fullbox(:, 1)), ...
+                        'x2', max(objs.annotation(j).fullbox(:, 1)), ...
+                        'y1', min(objs.annotation(j).fullbox(:, 2)), ...
+                        'y2', max(objs.annotation(j).fullbox(:, 2)), 'azimuth', 0, 'elevation', 0);
+                    
+            annos.obj_annos(end+1) = obj;
+        end
+    catch
+        continue;
+    end
+    
+    save(fullfile(annodir, annofile), '-struct', 'annos');
+end
+
+
+%  'sofa' 'table' 'bed' 'drawer' 'chair'  'sofa chair' 
+% 'Sofa', 'Table', 'Bed', 'Side Table', 'Chair', 'Sofa'
