1e0a36b24953a690370b9079f2fd30d7d85087aa
Wongun Choi <wgchoi@gmail.com>
2012-05-28 17:19:57 -0400

    missing file

---
+++ IndoorLayoutUnderstanding/objmodel/sync_objmodel.m
@@ -0,0 +1,41 @@
+function out = sync_objmodel(anno)
+om = objmodels();
+
+out = anno;
+out.objmodel = om;
+out.objtypes = {};
+out.objs = {};
+out.poses = {};
+out.obj_annos(:) = [];
+
+for i = 1:length(anno.objtypes)
+    idx = -1;
+    for j = 1:length(om)
+        if(strcmp(anno.objtypes{i}, om(j).name))
+            idx = j;
+            break;
+        end
+    end
+    
+    if(idx > 0)
+        out.objtypes{idx} = anno.objtypes{i};
+        out.objs{idx} = anno.objs{i};
+        if length(anno.poses) >= i
+            out.poses{idx} = anno.poses{i};
+        else
+            out.poses{idx} = [];
+            assert(isempty(anno.objs{i}));
+        end
+        
+        
+        for j = 1:length(anno.obj_annos)
+            if(anno.obj_annos(j).objtype == i)
+                temp = anno.obj_annos(j);
+                temp.objtype = idx;
+                out.obj_annos(end+1) = temp;
+            end
+        end
+    end
+end
+
+end
